61f6357107b9edc391b8b8f794b86e5c60750728
NE-O <boopathi_s@outlook.in>
2023-02-09 21:26:51 +0530

    Sqlite database intergrated

--- app.js
+++ app.js
@@ -4,22 +4,45 @@ const express = require("express")
 const app = express()
 const PORT = 8080
 
+var logined = false
+var name = ''
+var email = ''
+var passwd = ''
+
+
 app.use(express.urlencoded()) // req.body would give data in json formate
 app.set('view engine','ejs')
 
 
 app.get('/home',(req,res) => {
-    res.status(200,"ok")
-    res.render('home',{
-        PORT:PORT
-    })
+    if (logined) {
+        res.status(200,"ok")
+        res.render('home',{
+            PORT:PORT ,
+            name:name ,
+            login:logined
+        })
+        
+    }
+})
+
+
+app.get('/',(req,res) => {
+    res.redirect('/home')
 })
 
 app.get("/login", (req,res) => {
     res.render('login')
 })
+
 app.post("/login", (req,res) => {
     //post request
+    name = req.body.name
+    passwd = req.body.passwd
+    logined= true
+    console.log(req.body)
+    res.redirect('/')
+    res.send()
 })
 
 app.get("/signup", (req,res) => {
@@ -27,7 +50,8 @@ app.get("/signup", (req,res) => {
 })
 
 app.post("/signup", (req,res) => {
-    // post request
+    res.status(300,"redirecting to home")
+    res.redirect('/home')
 })
 
 app.listen(PORT,() => {
